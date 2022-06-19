dafa7d643702301b5b8a26b3c7edc69a5d686a33
devdan <oluochdancun19@gmail.com>
2022-06-19 09:46:29 +0300

    todo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  
+  const addedtaskList = document.getElementById("tasks");
+
+  
+  newTaskForm.addEventListener("submit", addTask);
 });
+
+const addTask = event => {
+  event.preventDefault();
+  const description = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = description.value;
+
+  appendTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
